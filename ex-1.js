3f3c1aa0f52ad6803e39fd46cac8519571ce7751
nueng <wongsaokrn.kump@gmail.com>
2024-07-17 14:28:57 +0700

    feat:add answer

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,21 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+	let left = 0
+	let right = students.length - 1
+	let index = -1 
+	while(left<=right){
+		let mid = Math.floor((left+right)/2)
+		if(students[mid] === searchStudent){
+			index = mid
+			break
+		}else if (students[mid]<searchStudent){
+			left = mid +1
+		}else{
+			right = mid -1
+		}
+	}
+	return index
 }
 
 // ตอบคำถามตรงนี้จ้า
+ let students = ["Andrew", "Bobby", "Diana", "George", "Hannah", "Isaac", "Jasmine", "John"]
+ console.log(findStudentIndex(students,"John"));
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,31 @@
+
 function findBookIndex(books, searchTitle) {
-  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0
+  let right = books.length -1
+  let index = -1
+  while(left<=right){
+    let mid = Math.floor((left+right)/2)
+    
+    if(books[mid].title === searchTitle){
+      index = mid
+      break
+    }else if(books[mid].title<searchTitle){
+      left = mid +1
+    }else{
+      right = mid -1
+    }
+  }
+  return index
 }
+let book = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" }
+];
+
+console.log(findBookIndex(book,"The Master Mind"));
